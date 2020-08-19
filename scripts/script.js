d73e3e8afcc6c18d852132f79a4e4a919c506ada
proforty7 <proforty7@gmail.com>
2020-08-19 19:10:02 +0530

    add toggle for desktop
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const toggleShare = () => {
   const share = document.querySelector(".share");
+  const tooltip = document.querySelector(".share-buttons-container");
   share.classList.toggle("showTooltip");
+  tooltip.classList.toggle("show-tooltip");
 };
